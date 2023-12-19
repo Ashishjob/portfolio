336444afd32c23c147d2c8d40fe4ea3787b78246
Ashishjob <ashishjob104@gmail.com>
2023-12-19 13:56:12 -0600

    feat: make small design changes

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,9 +5,10 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const projects = [
   {
-    image: "/portfolioProject.svg",
+    image: "/portfolioImage.svg",
     title: "portfolio",
-    description: "responsive website built to showcase projects and experience.",
+    description:
+      "responsive website built to showcase projects and experience.",
     icons: ["nextjs", "ts", "tailwind", "figma"],
     url: "https://github.com/Ashishjob/HorizontalPortfolioWebsite",
     demo: "https://ashishjob.com",
@@ -32,58 +33,49 @@ const projects = [
     demo: "",
     code: "",
   },
-  
 ];
 
 export default function Projects() {
-    return (
-      <main className="flex flex-col items-center h-screen">
-        <div className=" text-2xl md:text-4xl underline items-center justify-center flex flex-row mb-16">
+  return (
+    <main className="flex flex-col items-center h-screen">
+      <div className=" text-2xl md:text-4xl underline items-center justify-center flex flex-row mb-16">
         Projects
       </div>
       <Carousel showThumbs={false} className="h-1/2 w-full lg:w-1/2">
-      {projects.map((project, index) => (
-  <div key={index} className="flex items-center justify-center h-full">
-    <div className="w-1/2">
-      <Image
-        className="select-none"
-        src={project.image}
-        alt="Project Image"
-        width={50}
-        height={50}
-      />
-    </div>
-    <div className="flex flex-col items-start justify-center w-1/2 text-xs lg:text-xl">
-      <p>{project.description}</p>
-      <ul className="flex flex-row justify-evenly w-full">
-        {project.icons.map((icon, icon_ind) => (
-          <li key={icon_ind} className="w-8 h-8">
-            <Image
-              src={`/${icon}.svg`}
-              width={50}
-              height={50}
-              alt="icon"
-              className="m-3"
-            />
-          </li>
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className="flex flex-col lg:flex-row items-center justify-center h-full"
+          >
+            <div className="w-full lg:w-1/2">
+              <Image
+                className="select-none"
+                src={project.image}
+                alt="Project Image"
+                width={50}
+                height={50}
+              />
+            </div>
+            <div className="flex flex-col items-center justify-center w-full lg:w-1/2 text-xs md:text-base">
+            <h2 className="mb-4 text-sm md:text-xl underline">{project.title}</h2>
+              <p className="mb-4">{project.description}</p>
+              <ul className="flex flex-row justify-evenly w-full text-xs">
+                {project.icons.map((icon, icon_ind) => (
+                  <li key={icon_ind} className="w-8 h-8">
+                    <Image
+                      src={`/${icon}.svg`}
+                      width={50}
+                      height={50}
+                      alt="icon"
+                      className="m-3"
+                    />
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         ))}
-      </ul>
-      <div className="flex justify-evenly items-center mt-8 w-full">
-  {project.demo && (
-    <button className="hover:bg-red rounded-lg text-primary bg-light-black p-2" onClick={() => window.open(project.demo, "_blank")}>
-      View Demo
-    </button>
-  )}
-  {project.code && (
-    <button className="hover:bg-red rounded-lg text-primary bg-light-black p-2" onClick={() => window.open(project.code, "_blank")}>
-      View Code
-    </button>
-  )}
-</div>
-    </div>
-  </div>
-))}
       </Carousel>
     </main>
-    );
-  }
+  );
+}
