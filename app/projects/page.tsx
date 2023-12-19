eb88ed4f4bb3268d5ddc526bb2176b4b5d2db99e
Ashishjob <ashishjob104@gmail.com>
2023-12-19 15:22:24 -0600

    testing slick

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 const projectData = [
   {
