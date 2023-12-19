5188c53906e425b9609365fff280656bfc4fb2d9
Ashishjob <ashishjob104@gmail.com>
2023-12-19 00:08:58 -0600

    refactor: update experiences component casing ```

--- app/components/Experiences.tsx
+++ app/components/Experiences.tsx
@@ -1,24 +1,24 @@
 export const experiences = [
     {
-        position: "incoming engineering technician i - nasa",
-        time: "[ starting january 2024 ]",
+        position: "Incoming Engineering Technician I - NASA",
+        time: "[ Starting January 2024 ]",
         url: "https://www.nasa.gov/johnson/",
-        description: "details coming soon..."
+        description: "Details coming soon..."
     },    {
-        position: "academic support assistant - university of houston",
-        time: "[ august 2023 - present ]",
+        position: "Academic Support Assistant - University of Houston",
+        time: "[ August 2023 - Present ]",
         url: "https://www.uh.edu/nsm/computer-science/",
         description: "I support 250+ students in structured procedural programming and elementary functions in Python through code-reviewing, and provided feedback for multiple Python programming assignments."
     },
     {
-        position: "undergraduate research assistant - sail lab",
-        time: "[ may 2023 - september 2023 ]",
+        position: "Undergraduate Research Assistant - SAIL Lab",
+        time: "[ May 2023 - September 2023 ]",
         url: "https://sail.cive.uh.edu/",
-        description: "I utilized python, NeRFstudio, and LERF to acquire and analyze data for computer vision and language modeling in order to detect faults in structures caused by natural disasters."
+        description: "I utilized Python, NeRFstudio, and LERF to acquire and analyze data for computer vision and language modeling in order to detect faults in structures caused by natural disasters."
     },
     {
-        position: "cybersecurity intern - cyvergance.ai",
-        time: "[ march 2023 - may 2023 ]",
+        position: "Cybersecurity Intern - Cyvergance.AI",
+        time: "[ March 2023 - May 2023 ]",
         url: "https://cyvergance.ai/",
         description: "I evaluated the institution's threat feed monitoring policies, identifying vulnerabilities to enhance cybersecurity, and I led a team promoting collaboration among all members."
     }
