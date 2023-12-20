82f757a205cb5fac502f4ebac5531f1972c8eddd
Ashishjob <ashishjob104@gmail.com>
2023-12-19 22:32:54 -0600

    feat: fix font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import localFont from 'next/font/local'
 
-const inter = Inter({ subsets: ['latin'] })
+const myFont = localFont({ src: './fonts/RobotInvaders.ttf' })
+
 
 export const metadata: Metadata = {
   title: 'Ashish Job Portfolio Website',
@@ -23,7 +25,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={myFont.className}>{children}</body>
     </html>
   )
 }
