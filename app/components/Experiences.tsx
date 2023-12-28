79d08ade0864bbbc5980cc3c62046c3b6d29fda4
Ashishjob <ashishjob104@gmail.com>
2023-12-27 22:24:43 -0600

    refactor: update job title

--- app/components/Experiences.tsx
+++ app/components/Experiences.tsx
@@ -1,6 +1,6 @@
 export const experiences = [
     {
-        position: "Incoming Engineering Technician I - NASA",
+        position: "Incoming Machine Learning Intern - NASA",
         time: "[ Starting January 2024 ]",
         url: "https://www.nasa.gov/johnson/",
         description: "I will be working on the Integrated Mission Operation Contract III (IMOC III) Avionics Systems Team, but more details coming soon!"
